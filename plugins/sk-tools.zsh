00f44917b3e17935206c07cedd850f859960e597
lilydjwg <lilydjwg@gmail.com>
2018-09-16 00:43:07 +0800

    sk: better UI display

--- plugins/sk-tools.zsh
+++ plugins/sk-tools.zsh
@@ -1,8 +1,34 @@
+__cursor_pos () {
+  local pos
+  exec {tty}<>/dev/tty
+  echo -n '\e[6n' >&$tty; read -rsdR pos <&$tty
+  exec {tty}>&-
+  [[ $pos =~ '([0-9]+);([0-9]+)$' ]]
+  print $match[1] $match[2]
+}
+
+__calc_height () {
+  local pos
+  typeset -i height left want
+  pos=($(__cursor_pos))
+  left=$(( LINES - pos[1] ))
+  want=$(( LINES * 0.4 ))
+  if (( left > want )); then
+    height=$left
+  else
+    height=$want
+  fi
+  height=$(( height + 1)) # the prompt line is used too
+  print $height
+}
+
 sk-vim-mru () {
-  local file
-  file=$(tail -n +2 ~/.vim/vim_mru_files | sk)
+  local file cmd
+  cmd=${1:-vim}
+  file=$(tail -n +2 ~/.vim/vim_mru_files | \
+    sk --height $(__calc_height) --reverse -p "$cmd> ")
   if [[ -n $file ]]; then
-    ${1:-vim} $file
+    ${=cmd} $file
   else
     return 130
   fi
@@ -10,20 +36,26 @@ sk-vim-mru () {
 
 sk-search-history () {
   local cmd
-  cmd=$(history -n 1 | sk)
+  cmd=$(history -n 1 | \
+    sk --height $(__calc_height) --reverse -p 'cmd> ')
   if [[ -n $cmd ]]; then
     BUFFER=$cmd
     (( CURSOR = $#BUFFER ))
+    zle redisplay # for syntax highlight
   fi
 }
 
 sk-cd () {
   local dir
-  dir=$(sort -nr ~/.local/share/autojump/autojump.txt | awk '{print $2}' | sk)
+  dir=$(sort -nr ~/.local/share/autojump/autojump.txt | \
+    awk '{print $2}' | \
+    sk --height $(__calc_height) --reverse -p 'cd> ')
   if [[ -n $dir ]]; then
     zle push-line
     BUFFER="cd ${(q)dir}"
     zle accept-line
+  else
+    zle redisplay
   fi
 }
 
@@ -37,6 +69,10 @@ if (( $+commands[sk] )); then
   vim-mru () { sk-vim-mru }
   if (( $+commands[vv] )); then
     vv-mru () { sk-vim-mru vv }
+  fi
+
+  if [[ -f /usr/share/skim/completion.zsh ]]; then
+    . /usr/share/skim/completion.zsh
   fi
 fi
 
