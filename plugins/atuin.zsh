11a80307e27e7ac5d5639bc0517745d4119c83a1
lilydjwg <lilydjwg@gmail.com>
2023-12-31 12:52:18 +0800

    atuin: use up-down layout and try to keep cursor position

--- plugins/atuin.zsh
+++ plugins/atuin.zsh
@@ -1,3 +1,93 @@
-eval "$(ATUIN_NOBIND="true" atuin init zsh)"
+# shellcheck disable=SC2034,SC2153,SC2086,SC2155
+
+# Above line is because shellcheck doesn't support zsh, per
+# https://github.com/koalaman/shellcheck/wiki/SC1071, and the ignore: param in
+# ludeeus/action-shellcheck only supports _directories_, not _files_. So
+# instead, we manually add any error the shellcheck step finds in the file to
+# the above line ...
+
+__cursor_pos () {
+  local pos
+  exec {tty}<>/dev/tty
+  echo -n '\e[6n' >&$tty; read -rsdR pos <&$tty
+  exec {tty}>&-
+  [[ $pos =~ '([0-9]+);([0-9]+)$' ]]
+  print $match[1] $match[2]
+}
+
+__calc_height () {
+  local pos
+  typeset -i height left want
+  pos=($(__cursor_pos))
+  left=$(( LINES - pos[1] ))
+  want=$(( LINES * 0.4 ))
+  if (( left > want )); then
+    height=$left
+  else
+    height=$want
+  fi
+  height=$(( height + 1)) # the prompt line is used too
+  print $height
+}
+
+# Source this in your ~/.zshrc
+autoload -U add-zsh-hook
+
+export ATUIN_SESSION=$(atuin uuid)
+
+_atuin_preexec() {
+    local id
+    id=$(atuin history start -- "$1")
+    export ATUIN_HISTORY_ID="$id"
+}
+
+_atuin_precmd() {
+    local EXIT="$?"
+
+    [[ -z "${ATUIN_HISTORY_ID:-}" ]] && return
+
+    (ATUIN_LOG=error atuin history end --exit $EXIT -- $ATUIN_HISTORY_ID &) >/dev/null 2>&1
+    export ATUIN_HISTORY_ID=""
+}
+
+_atuin_search() {
+    emulate -L zsh
+    zle -I
+
+    # swap stderr and stdout, so that the tui stuff works
+    # TODO: not this
+    # shellcheck disable=SC2048
+    output=$(ATUIN_SHELL_ZSH=t ATUIN_LOG=error atuin search $* -i --inline-height $(__calc_height) -- $BUFFER 3>&1 1>&2 2>&3)
+
+    zle reset-prompt
+
+    if [[ -n $output ]]; then
+        RBUFFER=""
+        LBUFFER=$output
+    fi
+
+    if [[ $LBUFFER == __atuin_accept__:* ]]
+    then
+        LBUFFER=${LBUFFER#__atuin_accept__:}
+        zle accept-line
+    else
+        zle infer-next-history && zle up-history
+    fi
+}
+
+_atuin_up_search() {
+    # Only trigger if the buffer is a single line
+    if [[ ! $BUFFER == *$'\n'* ]]; then
+        _atuin_search --shell-up-key-binding
+    else
+        zle up-line
+    fi
+}
+
+add-zsh-hook preexec _atuin_preexec
+add-zsh-hook precmd _atuin_precmd
+
+zle -N _atuin_search_widget _atuin_search
+zle -N _atuin_up_search_widget _atuin_up_search
 
 bindkey -M emacs "\esa" _atuin_search_widget
